134ecbe9a1575d7c7fdc965d98cd9e83df4d55e2
Ademir <ademir@debian.local>
2023-06-30 18:16:20 -0300

    stns

--- src/components/utils/formatDate/index.tsx
+++ src/components/utils/formatDate/index.tsx
@@ -2,7 +2,7 @@ import {format , parseISO} from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
 export const  FormatDate = (date:string ) => {
-    return format (parseISO(date), "dd 'de' MMMM 'de' yyy", {
+    return format (parseISO(date), "'Dia' dd 'de' MMMM', às ' HH:mm'h'", {
         locale: ptBR
     })
 }
